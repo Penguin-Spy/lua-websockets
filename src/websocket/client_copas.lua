e7b089c5876e4306392365f3c584da7481221a9c
Penguin_Spy <penguinspy2435@outlook.com>
2022-10-05 22:14:39 -0700

    fix copas client (caused by lua 5.4 i think) also rip git diff lol

--- src/websocket/client_copas.lua
+++ src/websocket/client_copas.lua
@@ -1,38 +1,39 @@
-local socket = require'socket'
-local sync = require'websocket.sync'
-local tools = require'websocket.tools'
+local socket = require 'socket'
+local sync = require 'websocket.sync'
+local tools = require 'websocket.tools'
 
 local new = function(ws)
   ws = ws or {}
-  local copas = require'copas'
-  
+  local copas = require 'copas'
+
   local self = {}
-  
-  self.sock_connect = function(self,host,port)
-    self.sock = socket.tcp()
+
+  self.sock_connect = function(self, host, port, ssl_params)
+    self.sock = copas.wrap(socket.tcp(), ssl_params)
     if ws.timeout ~= nil then
       self.sock:settimeout(ws.timeout)
     end
-    local _,err = copas.connect(self.sock,host,port)
+    local _, err = self.sock:connect(host, port)
     if err and err ~= 'already connected' then
       self.sock:close()
-      return nil,err
+      return nil, err
     end
   end
-  
-  self.sock_send = function(self,...)
-    return copas.send(self.sock,...)
+
+  self.sock_send = function(self, ...)
+    return self.sock:send(...)
   end
-  
-  self.sock_receive = function(self,...)
-    return copas.receive(self.sock,...)
+
+  self.sock_receive = function(self, ...)
+    return self.sock:receive(...)
   end
-  
+
   self.sock_close = function(self)
     self.sock:shutdown()
     self.sock:close()
   end
-  
+
+  self.copas = true
   self = sync.extend(self)
   return self
 end
--- src/websocket/sync.lua
+++ src/websocket/sync.lua
@@ -1,51 +1,51 @@
-local frame = require'websocket.frame'
-local handshake = require'websocket.handshake'
-local tools = require'websocket.tools'
-local ssl = require'ssl'
+local frame = require 'websocket.frame'
+local handshake = require 'websocket.handshake'
+local tools = require 'websocket.tools'
+local ssl = require 'ssl'
 local tinsert = table.insert
 local tconcat = table.concat
 
 local receive = function(self)
   if self.state ~= 'OPEN' and not self.is_closing then
-    return nil,nil,false,1006,'wrong state'
+    return nil, nil, false, 1006, 'wrong state'
   end
   local first_opcode
   local frames
   local bytes = 3
   local encoded = ''
-  local clean = function(was_clean,code,reason)
+  local clean = function(was_clean, code, reason)
     self.state = 'CLOSED'
     self:sock_close()
     if self.on_close then
       self:on_close()
     end
-    return nil,nil,was_clean,code,reason or 'closed'
+    return nil, nil, was_clean, code, reason or 'closed'
   end
   while true do
-    local chunk,err = self:sock_receive(bytes)
+    local chunk, err = self:sock_receive(bytes)
     if err then
-      return clean(false,1006,err)
+      return clean(false, 1006, err)
     end
-    encoded = encoded..chunk
-    local decoded,fin,opcode,_,masked = frame.decode(encoded)
+    encoded = encoded .. chunk
+    local decoded, fin, opcode, _, masked = frame.decode(encoded)
     if not self.is_server and masked then
-      return clean(false,1006,'Websocket receive failed: frame was not masked')
+      return clean(false, 1006, 'Websocket receive failed: frame was not masked')
     end
     if decoded then
       if opcode == frame.CLOSE then
         if not self.is_closing then
-          local code,reason = frame.decode_close(decoded)
+          local code, reason = frame.decode_close(decoded)
           -- echo code
           local msg = frame.encode_close(code)
-          local encoded = frame.encode(msg,frame.CLOSE,not self.is_server)
-          local n,err = self:sock_send(encoded)
+          local encoded = frame.encode(msg, frame.CLOSE, not self.is_server)
+          local n, err = self:sock_send(encoded)
           if n == #encoded then
-            return clean(true,code,reason)
+            return clean(true, code, reason)
           else
-            return clean(false,code,err)
+            return clean(false, code, err)
           end
         else
-          return decoded,opcode
+          return decoded, opcode
         end
       end
       if not first_opcode then
@@ -55,55 +55,55 @@ local receive = function(self)
         if not frames then
           frames = {}
         elseif opcode ~= frame.CONTINUATION then
-          return clean(false,1002,'protocol error')
+          return clean(false, 1002, 'protocol error')
         end
         bytes = 3
         encoded = ''
-        tinsert(frames,decoded)
+        tinsert(frames, decoded)
       elseif not frames then
-        return decoded,first_opcode
+        return decoded, first_opcode
       else
-        tinsert(frames,decoded)
-        return tconcat(frames),first_opcode
+        tinsert(frames, decoded)
+        return tconcat(frames), first_opcode
       end
     else
       assert(type(fin) == 'number' and fin > 0)
       bytes = fin
     end
   end
-  assert(false,'never reach here')
+  assert(false, 'never reach here')
 end
 
-local send = function(self,data,opcode)
+local send = function(self, data, opcode)
   if self.state ~= 'OPEN' then
-    return nil,false,1006,'wrong state'
+    return nil, false, 1006, 'wrong state'
   end
-  local encoded = frame.encode(data,opcode or frame.TEXT,not self.is_server)
-  local n,err = self:sock_send(encoded)
+  local encoded = frame.encode(data, opcode or frame.TEXT, not self.is_server)
+  local n, err = self:sock_send(encoded)
   if n ~= #encoded then
-    return nil,self:close(1006,err)
+    return nil, self:close(1006, err)
   end
   return true
 end
 
-local close = function(self,code,reason)
+local close = function(self, code, reason)
   if self.state ~= 'OPEN' then
-    return false,1006,'wrong state'
+    return false, 1006, 'wrong state'
   end
   if self.state == 'CLOSED' then
-    return false,1006,'wrong state'
+    return false, 1006, 'wrong state'
   end
-  local msg = frame.encode_close(code or 1000,reason)
-  local encoded = frame.encode(msg,frame.CLOSE,not self.is_server)
-  local n,err = self:sock_send(encoded)
+  local msg = frame.encode_close(code or 1000, reason)
+  local encoded = frame.encode(msg, frame.CLOSE, not self.is_server)
+  local n, err = self:sock_send(encoded)
   local was_clean = false
   local code = 1005
   local reason = ''
   if n == #encoded then
     self.is_closing = true
-    local rmsg,opcode = self:receive()
+    local rmsg, opcode = self:receive()
     if rmsg and opcode == frame.CLOSE then
-      code,reason = frame.decode_close(rmsg)
+      code, reason = frame.decode_close(rmsg)
       was_clean = true
     end
   else
@@ -114,61 +114,62 @@ local close = function(self,code,reason)
     self:on_close()
   end
   self.state = 'CLOSED'
-  return was_clean,code,reason or ''
+  return was_clean, code, reason or ''
 end
 
-local connect = function(self,ws_url,ws_protocol,ssl_params)
+local connect = function(self, ws_url, ws_protocol, ssl_params)
   if self.state ~= 'CLOSED' then
-    return nil,'wrong state',nil
+    return nil, 'wrong state', nil
   end
-  local protocol,host,port,uri = tools.parse_url(ws_url)
+  local protocol, host, port, uri = tools.parse_url(ws_url)
   -- Preconnect (for SSL if needed)
-  local _,err = self:sock_connect(host,port)
+  local _, err = self:sock_connect(host, port, ssl_params)
   if err then
-    return nil,err,nil
+    return nil, err, nil
   end
-  if protocol == 'wss' then
-    self.sock = ssl.wrap(self.sock, ssl_params)
-    self.sock:dohandshake()
+  if protocol == 'wss' then -- copas sockets have already completed the handshake
+    if not self.copas then
+      self.sock = ssl.wrap(self.sock, ssl_params)
+      self.sock:dohandshake()
+    end
   elseif protocol ~= "ws" then
     return nil, 'bad protocol'
   end
-  local ws_protocols_tbl = {''}
+  local ws_protocols_tbl = { '' }
   if type(ws_protocol) == 'string' then
-      ws_protocols_tbl = {ws_protocol}
+    ws_protocols_tbl = { ws_protocol }
   elseif type(ws_protocol) == 'table' then
-      ws_protocols_tbl = ws_protocol
+    ws_protocols_tbl = ws_protocol
   end
   local key = tools.generate_key()
-  local req = handshake.upgrade_request
-  {
+  local req = handshake.upgrade_request {
     key = key,
     host = host,
     port = port,
     protocols = ws_protocols_tbl,
     uri = uri
   }
-  local n,err = self:sock_send(req)
+  local n, err = self:sock_send(req)
   if n ~= #req then
-    return nil,err,nil
+    return nil, err, nil
   end
   local resp = {}
   repeat
-    local line,err = self:sock_receive('*l')
-    resp[#resp+1] = line
+    local line, err = self:sock_receive('*l')
+    resp[#resp + 1] = line
     if err then
-      return nil,err,nil
+      return nil, err, nil
     end
   until line == ''
-  local response = table.concat(resp,'\r\n')
+  local response = table.concat(resp, '\r\n')
   local headers = handshake.http_headers(response)
   local expected_accept = handshake.sec_websocket_accept(key)
   if headers['sec-websocket-accept'] ~= expected_accept then
     local msg = 'Websocket Handshake failed: Invalid Sec-Websocket-Accept (expected %s got %s)'
-    return nil,msg:format(expected_accept,headers['sec-websocket-accept'] or 'nil'),headers
+    return nil, msg:format(expected_accept, headers['sec-websocket-accept'] or 'nil'), headers
   end
   self.state = 'OPEN'
-  return true,headers['sec-websocket-protocol'],headers
+  return true, headers['sec-websocket-protocol'], headers
 end
 
 local extend = function(obj)
@@ -177,10 +178,10 @@ local extend = function(obj)
   assert(obj.sock_close)
 
   assert(obj.is_closing == nil)
-  assert(obj.receive    == nil)
-  assert(obj.send       == nil)
-  assert(obj.close      == nil)
-  assert(obj.connect    == nil)
+  assert(obj.receive == nil)
+  assert(obj.send == nil)
+  assert(obj.close == nil)
+  assert(obj.connect == nil)
 
   if not obj.is_server then
     assert(obj.sock_connect)
